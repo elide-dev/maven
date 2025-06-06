fc19d5bcfe555472a4a5cd67a04fdf554f7b85c5
melodicore <datafox@datafox.me>
2025-06-06 21:27:48 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    kotlin("jvm") version "2.1.21"
+    `java-library`
+    `maven-publish`
+}
+
+group = "dev.elide"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+kotlin {
+    jvmToolchain(24)
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_1_8
+    }
+}
+
+dependencies {
+    implementation(libs.plexus.compiler.api)
+    implementation(libs.javax.inject)
+    implementation(libs.sisu.plexus)
+    implementation(libs.sisu.inject)
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "dev.elide"
+            artifactId = "elide-plexus-compilers"
+            version = version
+
+            from(components["kotlin"])
+        }
+    }
+}
